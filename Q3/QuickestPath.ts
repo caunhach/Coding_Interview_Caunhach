05346e8a97c97c7341ac023e6ab1abcbb2a554d1
caunhach <chayapon.aunh@gmail.com>
2024-06-09 12:48:39 +0700

    add test cases

--- Q3/QuickestPath.ts
+++ Q3/QuickestPath.ts
@@ -26,7 +26,7 @@ function quickestPath(board: Board): number[] {
 					continue;
 				if (ladders.has(newPos))
                     newPos = ladders.get(newPos)!;
-				if (snakes.has(newPos))
+				else if (snakes.has(newPos))
                     newPos = snakes.get(newPos)!;
 				if (!visitPos[newPos - 1]) {
 					visitPos[newPos - 1] = true
@@ -39,10 +39,34 @@ function quickestPath(board: Board): number[] {
 	return bfs()
 }
 
-// const board: Board = {
-//     ladders: [[3, 39], [14, 35], [31, 70], [44, 65], [47, 86], [63, 83], [71, 93]],
-//     snakes: [[21, 4], [30, 8], [55, 38], [79, 42], [87, 54], [91, 48], [96, 66]]
-// };
+// {
+// 	const testBoard: Board = {
+//     	ladders: [[1, 38], [14, 44], [27, 57], [40, 67], [50, 85], [65, 97]],
+//     	snakes: [[24, 7], [33, 19], [48, 30], [58, 42], [83, 63], [99, 77]]
+// 	};
+// 	console.log(quickestPath(testBoard));
+// }
 
-// const result = quickestPath(board);
-// console.log("Sequence of dice rolls:", result);
+// {
+// 	const testBoard: Board = {
+// 		ladders: [ [3, 39], [14, 35], [31, 70], [44, 65], [47, 86], [63, 83], [71, 93] ],
+// 		snakes: [ [21, 4], [30, 8], [55, 38], [79, 42], [87, 54], [91, 48], [96, 66] ]
+// 	}
+// 	console.log(quickestPath(testBoard));
+// }
+
+// {
+// 	const testBoard: Board = {
+// 		ladders: [[2, 38], [16, 29], [22, 58], [28, 79], [46, 96]],
+//         snakes: [[13, 5], [34, 20], [49, 32], [67, 51], [87, 64], [93, 69]]
+// 	}
+// 	console.log(quickestPath(testBoard));
+// }
+
+// {
+// 	const testBoard: Board = {
+// 		ladders: [[6, 20], [13, 99]],
+//         snakes: [[7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [21, 7], [22, 8], [23, 9], [24, 10], [25, 11], [26, 12]]
+// 	}
+// 	console.log(quickestPath(testBoard));
+// }
